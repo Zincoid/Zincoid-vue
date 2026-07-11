e2019b404f76643d2c459dbcdd2fc02e933e88e6
Zincoid <2660181154@qq.com>
2026-07-12 06:33:07 +0800

    修正

--- src/views/RepoDetailPage.vue
+++ src/views/RepoDetailPage.vue
@@ -300,7 +300,7 @@ async function saveEdit() {
 
     <MediaViewer :src="viewerSrc" :visible="viewerVisible" @close="viewerVisible = false" />
 
-    <label v-if="isOwner() && repo.type !== 0" class="upload-fab" :title="t('article.upload')">
+    <label v-if="repo && isOwner() && repo.type !== 0" class="upload-fab" :title="t('article.upload')">
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
       <input type="file" accept="image/*,video/*,audio/*" multiple class="hidden-input" @change="handleItemFiles" />
     </label>
